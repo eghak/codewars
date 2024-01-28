f4bcc219da88674da9145bd5663826c5409a2516
Egha Kusuma <eghakusuma1@gmail.com>
2024-01-27 22:38:21 -0700

    divisible

---
+++ 8kyu/divisible.js
@@ -0,0 +1,17 @@
+/*
+Complete the function which takes two arguments and returns all numbers which are divisible by the given divisor. First argument is an array of numbers and the second is the divisor.
+
+Example(Input1, Input2 --> Output)
+[1, 2, 3, 4, 5, 6], 2 --> [2, 4, 6]
+*/
+
+// Solution
+function divisibleBy(numbers, divisor) {
+  let res = [];
+
+  for (let i = 0; i < numbers.length; i++) {
+    numbers[i] % divisor === 0 ? res.push(numbers[i]) : null;
+  }
+
+  return res;
+}
