b33d2cd80de5a5c8aae4dbde5b4674b8f42bbfd9
Egha Kusuma <eghakusuma1@gmail.com>
2024-02-24 22:38:21 -0700

    newHealth added

---
+++ 8kyu/newHealth.js
@@ -0,0 +1,11 @@
+// Grasshopper - Terminal game combat function
+
+/*
+Create a combat function that takes the player's current health and the amount of damage recieved, and returns the player's new health. Health can't be less than 0.
+*/
+
+// Solution
+function combat(health, damage) {
+  let newHealth = health - damage;
+  return newHealth < 0 ? 0 : newHealth;
+}
